4d9ee34c353917832d51ca79ac26f6c7b6f22d85
youchenz <youchen_you@hotmail.com>
2016-12-28 00:13:27 +0100

    Añadida funcionalidades

--- calculadora.cc
+++ calculadora.cc
@@ -2,7 +2,21 @@
 
 using namespace std;
 
+float Suma(float a, float b) {
+	return a + b;
+}
 
+float Resta(float a, float b) {
+	return a - b;
+}
+
+float Multiplicacion(float a, float b) {
+	return a * b;
+}
+
+float Division(float a, float b) {
+	return a / b;
+}
 
 int main() {
 	
